35673b0a39079fb50524832fac7dc608b93a775c
the0last0wizard115 <57833353+the0last0wizard115@users.noreply.github.com>
2019-11-22 21:49:17 +0700

    Add files via upload

---
+++ FlyingZombie.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlantvsZombie
+{
+    public class FlyingZombie : Zombie
+    {
+        //private Texture2D _Image = Content.Load<Texture2D>("Texture/NormalZombie");
+        private float _ScaleFactor = .3f;
+        private Vector2 _Position;
+        private float _DamageFactor = 20;
+        private int counter = 0;
+        private int state = 0;
+        private Plant MeetPlant()
+        {
+            int pi, zi, pj, zj;
+            float a = PVZGame.Side;
+            foreach (var p in PVZGame.Game.Plants)
+            {
+                pi = Utility.GetCell(p.Position.X, a);
+                pj = Utility.GetCell(p.Position.Y, a);
+                zi = Utility.GetCell(Position.X, a);
+                zj = Utility.GetCell(Position.Y, a);
+
+                if (pi == zi && pj == zj) // Position.Y or Position.X - the side of a square
+                    return p;
+            } 
+            return null;
+        }
+
+        
+
+        public override void Update()
+        {
+            base.Update();
+            var p = MeetPlant();
+            if (p != null)
+            {
+                Console.WriteLine(counter);
+                if (counter == 0)
+                {
+                    counter++;
+                    float a = PVZGame.Side;
+                    MoveByCell(Utility.GetCell(p.Position.X, a), Utility.GetCell(p.Position.Y, a));
+                }
+                else
+                {
+                    state = 1;
+                    Attack(p);
+                }
+            } 
+            else Move();
+        }
+
+        public override void Attack(Plant p)
+        {
+            p.Damaged(_DamageFactor);
+        }
+
+        public override float GetScaleFactor()
+        {
+            return _ScaleFactor;
+        }
+
+        public override void Damaged(float dam)
+        {
+            if (state == 1)
+                Health -= dam;
+        }
+
+        public override void Move()
+        {
+            _Position.X = Position.X - Speed;
+            _Position.Y = Position.Y;
+            this.Position = _Position;
+        }
+
+        public void MoveByCell(int zx, int zy)
+        {
+            _Position.X = Position.X-PVZGame.Side;
+            _Position.Y = Position.Y;
+            Position = _Position;
+        }
+
+        public FlyingZombie()
+        {
+            _Position.X = 700;
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+            _Position.Y = r.Next(0, 300);
+            Position = _Position;
+            Speed = 0.2f;
+        }
+    }
+}
---
+++ PVZGame.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Timers;
+using System;
+
+namespace PlantvsZombie
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class PVZGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public HashSet<GameObject> ManagedObjects;
+        public HashSet<Plant> Plants;
+        public HashSet<Zombie> Zombies;
+        private Dictionary<String, Texture2D> _TextureAssets = new Dictionary<string, Texture2D>();
+        public Dictionary<String, Texture2D> TextureAssets
+        {
+            get
+            {
+                return _TextureAssets;
+            }
+
+        }
+        float timeSinceLastSpawn;
+
+        private Texture2D background;
+        private Vector2 _ObjectPosition;
+        private String _ObjectClassName;
+        private MouseState mouseState;
+        private Vector2 _PlantPosition;
+        public const float Side=50;
+        private float scalefact = 0.2f;
+
+        private PVZGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+        }
+
+        public static readonly PVZGame Game = new PVZGame();
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            ManagedObjects = new HashSet<GameObject>();
+            Plants = new HashSet<Plant>();
+            Zombies = new HashSet<Zombie>();
+            timeSinceLastSpawn = 0f;
+            SpawnZombie();
+            this.IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = Content.Load<Texture2D>("Texture/Frontyard");
+            _TextureAssets["NormalZombie"] = Content.Load<Texture2D>("Texture/NormalZombie");
+            _TextureAssets["PeaShooter"] = Content.Load<Texture2D>("Texture/PeaShooter");
+            _TextureAssets["FlyingZombie"] = Content.Load<Texture2D>("Texture/FlyingZombie");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            var currentObjects = new HashSet<GameObject>(ManagedObjects);
+
+            foreach (var ob in currentObjects)
+            {
+                ob.Update();
+
+            }
+
+            timeSinceLastSpawn += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (timeSinceLastSpawn >= 8f)
+            {
+                SpawnZombie();
+            }
+            mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                //checking  before Spawn
+                SpawnPlant(mouseState.X, mouseState.Y);
+            }
+
+
+            base.Update(gameTime);
+        }
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            Rectangle rec = new Rectangle(0, 0, 800, 480);
+            spriteBatch.Draw(background, rec, Color.White);
+            var currentObjects = new HashSet<GameObject>(ManagedObjects);
+
+            foreach (var ob in currentObjects)
+            {
+                ob.Update();
+                _ObjectPosition = ob.Position;
+                _ObjectClassName = ob.GetType().Name;
+                //ScaleFactor!
+                if (_ObjectClassName != null)
+                    spriteBatch.Draw(_TextureAssets[_ObjectClassName], _ObjectPosition, null, Color.White, 0f, Vector2.Zero, scalefact, SpriteEffects.None, 0f);
+            }
+            // TODO: Add your drawing code here
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+
+        public void SpawnZombie()
+        {
+            Zombie z = new NormalZombie();
+            Zombie f = new FlyingZombie();
+            z.Died += HandleDeadZombie;
+            f.Died += HandleDeadZombie;
+            ManagedObjects.Add(z);
+            ManagedObjects.Add(f);
+            Zombies.Add(z);
+            Zombies.Add(f);
+            timeSinceLastSpawn = 0f;
+        }
+
+        private void HandleDeadZombie(object self)
+        {
+            ManagedObjects.Remove((GameObject)self);
+            Zombies.Remove((Zombie)self);
+        }
+
+        public void SpawnPlant(int _X, int _Y)
+        {
+            _PlantPosition.X = _X;
+            _PlantPosition.Y = _Y;
+            Plant pl = new PeaShooter(_PlantPosition);
+            pl.Died += HandleDeadPlantObject;
+            ManagedObjects.Add(pl);
+            Plants.Add(pl);
+        }
+
+        private void HandleDeadPlantObject(object self)
+        {
+            ManagedObjects.Remove((GameObject)self);
+            Plants.Remove((Plant)self);
+        }
+
+        public void EndGame()
+        {
+            Exit(); //temporary
+        }
+
+    }
+}
