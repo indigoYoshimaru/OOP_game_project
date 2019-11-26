a2fdef91c511671065b864abdc4e92090ddb70eb
ngvan0807 <40973093+lordhanzo123@users.noreply.github.com>
2019-11-26 20:39:28 +0700

    Submit Sun Flower Class

---
+++ SunFlower.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+
+
+namespace PlantvsZombie
+{
+    public class SunFlower:Plant
+    {
+        private float _currentTime = 0f;
+        public override void Update() 
+        {
+            _currentTime += (float)PVZGame.Game.CurrentGameTime.ElapsedGameTime.TotalSeconds;
+            base.Update();
+            if(_currentTime >=10f)
+            {
+                PVZGame.SpawnSun(this);
+                _currentTime = 0f;
+            }
+        }
+        public SunFlower(Vector2 _Position)
+        {
+            Position = _Position;
+        }
+        public override void Damaged(float dam)
+        {
+            Health -= dam;
+        }
+    }
+}
