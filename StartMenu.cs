eae89bf2df47cfff3e00066c44c33e92a4db2702
the0last0wizard115 <57833353+the0last0wizard115@users.noreply.github.com>
2019-12-15 21:21:01 +0700

    Add files via upload

--- StartMenu.cs
+++ StartMenu.cs
@@ -10,48 +10,38 @@ namespace PlantvsZombie
 {
     public class StartMenu : ControlManager
     {
-
-        SpriteBatch _SpriteBatch;
         public StartMenu(PVZGame game) : base(game)
         {
-            _SpriteBatch = new SpriteBatch(GraphicsDevice);
         }
         public override void InitializeComponent()
         {
-            var button1 = new Button()
+            var startButton = new Button()
             {
                 Text = "Start Game",
                 Size = new Vector2(200, 50),
                 BackgroundColor = Color.DarkGreen,
                 Location = new Vector2(300, 150)
             };
-            button1.Clicked += button1_Clicked;
-            Controls.Add(button1);
+            startButton.Clicked += StartButton_Clicked;
+            Controls.Add(startButton);
 
-            var button2 = new Button()
+            var quitButton = new Button()
             {
                 Text = "Quit",
                 Size = new Vector2(200, 50),
                 BackgroundColor = Color.DarkGreen,
                 Location = new Vector2(300, 250)
             };
-            button2.Clicked += button2_Clicked;
-            Controls.Add(button2);
+            quitButton.Clicked += QuitButton_Clicked;
+            Controls.Add(quitButton);
         }
-        public void button1_Clicked(object sender, EventArgs e)
+        public void StartButton_Clicked(object sender, EventArgs e)
         {
-            Button btn = sender as Button;
-            //state = play
+            ((PVZGame)Game).EnterGame();
         }
-        public void button2_Clicked(object sender, EventArgs e)
+        public void QuitButton_Clicked(object sender, EventArgs e)
         {
-            Button btn = sender as Button;
-            //state = end
-        }
-        public override void Draw(GameTime gameTime)
-        {
-            //_SpriteBatch.Draw(_Background, rec, Color.White);
-            base.Draw(gameTime);
+            ((PVZGame)Game).EndGame();
         }
     }
 }
