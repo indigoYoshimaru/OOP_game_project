993d9154409c59861d7078bd40b4bcc58f522c29
hoangpham12345 <55367863+hoangpham12345@users.noreply.github.com>
2019-12-15 07:28:49 +0700

    Update method
    Add method to collect and calculate Sun
    Add method Update to Update all method in PlayerManager, the main program only need to call this

--- PlayerManager.cs
+++ PlayerManager.cs
@@ -1,27 +1,28 @@
 ﻿using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace PlantvsZombie
 {
     public class PlayerManager
     {
         private int score = 0;
-        private int highscore = 0;
-        public enum MouseIcon { NORMAL, SUNFLOWER, PEASHOOTER };    //icon of the mouse
-        private  MouseIcon mIcon;
+        private int highScore;
+        private int totalSun = 200;
+        public enum MouseIcon { NORMAL, SUNFLOWER, PEASHOOTER, CARNIVOROUSPLANT };    //icon of the mouse
+        private MouseIcon mIcon;
         private MouseState _CurrentMouseState;
         private MouseState _OldMouseState;
         private Tile _MouseTile;
-
+        
         public String PlantState { get; set; } = "";
 
         public int GetScore()
-        { 
+        {
             return score;
         }
 
@@ -30,10 +31,10 @@ namespace PlantvsZombie
             return mIcon;
         }
 
-        
+
         public void UpdateScore(Zombie z)
         {
-            if (z is NormalZombie )
+            if (z is NormalZombie)
             {
                 score += 5;
             }
@@ -47,19 +48,107 @@ namespace PlantvsZombie
             }
         }
 
+        //send signal to plant the correct type
+        public void Controller()
+        {
+
+            if (Keyboard.GetState().IsKeyDown(Keys.X) || (Mouse.GetState().RightButton == ButtonState.Pressed))
+            {
+                mIcon = MouseIcon.NORMAL;
+                PlantState = "";
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                mIcon = MouseIcon.SUNFLOWER;
+                PlantState = "SunFlower";
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                mIcon = MouseIcon.PEASHOOTER;
+                PlantState = "PeaShooter";
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.C))
+            {
+                mIcon = MouseIcon.CARNIVOROUSPLANT;
+                PlantState = "CarnivorousPlant";
+            }
+
+        }
+        public void UpdateControl()
+        {
+            Controller();
+            _CurrentMouseState = Mouse.GetState();
+            if (_CurrentMouseState.LeftButton == ButtonState.Pressed && _OldMouseState.LeftButton == ButtonState.Released)
+            {
+
+                _MouseTile = PVZGame.Game.GameMap.GetTileAt(_CurrentMouseState.Position.ToVector2());
+                {
+                    if(SpendSun())
+                        PVZGame.Game.Spawner.SpawnPlant(_MouseTile);
+                    
+                }
+            }
+            _OldMouseState = _CurrentMouseState;
+        }
+
+        public bool SpendSun()
+        {
+            int sunSpend = 0;
+            switch (PlantState)
+            {
+                case "SunFlower":
+                    sunSpend = 50;
+                    break;
+                case "PeaShooter":
+                    sunSpend = 100;
+                    break;
+                case "CarnivorousPlant":
+                    sunSpend = 150;
+                    break;
+            }
+
+            if (totalSun >= sunSpend)
+            {
+                totalSun -= sunSpend;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void CheckSun()
+        {
+            if (mIcon != MouseIcon.NORMAL)
+                return;
+
+            if (Mouse.GetState().LeftButton != ButtonState.Pressed)
+                return;
+
+            foreach(GameObject o in PVZGame.Game.ManagedObjects.ToList())
+            {
+                if(o is Sun)
+                    totalSun += ((Sun) o).Collect(Mouse.GetState().X, Mouse.GetState().Y);
+            }
+        }
+
+        public int GetTotalSun()
+        {
+            return totalSun;
+        }
+
         public int GetHighScore()
         {
-            return highscore;
+            return highScore;
         }
 
         public void SetHighScore(int value)
         {
-            highscore = value;
+            highScore = value;
         }
 
         public void UpdateHighScore()
         {
-            if (score > highscore)
+            if (score > highScore)
             {
                 SetHighScore(score);
             }
@@ -70,9 +159,9 @@ namespace PlantvsZombie
             try
             {
                 var text = File.ReadAllText("highscore.txt", Encoding.UTF8);
-                if (!Int32.TryParse(text, out highscore))
+                if (!Int32.TryParse(text, out highScore))
                 {
-                    SetHighScore(-1);
+                    SetHighScore(0);
                 }
             }
             catch (Exception e)
@@ -86,7 +175,7 @@ namespace PlantvsZombie
             try
             {
                 //Open the File
-                StreamWriter sw = new StreamWriter("highscore.txt", false, Encoding.UTF8);
+                StreamWriter sw = new StreamWriter("highscore.txt", false, Encoding.UTF8); //false means rewrite the file
 
                 sw.WriteLine(GetHighScore());
 
@@ -99,41 +188,17 @@ namespace PlantvsZombie
             }
         }
 
-        //send signal to plant the correct type
-        public void Controller()
+        public void Update()
         {
-            
-            if (Keyboard.GetState().IsKeyDown(Keys.X) || (Mouse.GetState().RightButton == ButtonState.Pressed))
-            {
-                mIcon = MouseIcon.NORMAL;
-                PlantState = "";
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                mIcon = MouseIcon.SUNFLOWER;
-                PlantState = "SunFlower";
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.S))
-            {
-                mIcon = MouseIcon.PEASHOOTER;
-                PlantState = "PeaShooter";
-            }
-            
-        }
+            //call player Update()
+            UpdateControl();
 
-        public void UpdateControl()
-        {
-            Controller();
-            _CurrentMouseState = Mouse.GetState();
-            if (_CurrentMouseState.LeftButton == ButtonState.Pressed && _OldMouseState.LeftButton == ButtonState.Released)
-            {
+            //update highscore
+            UpdateHighScore();
 
-                _MouseTile = PVZGame.Game.GameMap.GetTileAt(_CurrentMouseState.Position.ToVector2());
-                {
-                    PVZGame.Game.Spawner.SpawnPlant(_MouseTile);
-                }
-            }
-            _OldMouseState = _CurrentMouseState;
+            //update sun
+            CheckSun();
+
         }
     }
 }
