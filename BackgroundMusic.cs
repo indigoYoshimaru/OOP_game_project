dc97410fac0e75e54639ba9d38f87337f20d6692
hoangpham12345 <55367863+hoangpham12345@users.noreply.github.com>
2019-12-30 20:33:07 +0700

    add method to play sound on each state of the game

---
+++ BackgroundMusic.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantvsZombie
+{
+    public class BackgroundMusic
+    {
+        private Song _MenuSong;
+        private Song _GameSong;
+        private Song _EndSong;
+        
+        public void LoadContent()
+        {
+            _MenuSong = PVZGame.Game.Content.Load<Song>("Texture/Music/background1");
+            _GameSong = PVZGame.Game.Content.Load<Song>("Texture/Music/background2");
+            _EndSong = PVZGame.Game.Content.Load<Song>("Texture/Music/background3");
+            MediaPlayer.IsRepeating = true;
+        }
+
+        public void Update()
+        {
+            switch (PVZGame.Game.State)
+            {
+                case PVZGame.GameState.START_MENU:
+                    Play(_MenuSong);
+                    break;
+                case PVZGame.GameState.PLAYING:
+                    Play(_GameSong);
+                    break;
+                case PVZGame.GameState.END_MENU:
+                    Play(_EndSong);
+                    break;
+            }
+
+        }
+
+        public void OnChangeState()
+        {
+            MediaPlayer.Stop();
+        }
+
+        public void Play(Song s)
+        {
+            if (MediaPlayer.State == MediaState.Stopped)
+            {
+                MediaPlayer.Play(s);
+            }
+        }
+    }
+}
